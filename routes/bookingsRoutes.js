00ddb705f07bd4e913d420dcbeee3cdc07051b81
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-05-26 12:18:14 +0530

    Authentication for contact and booking page is completed...

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -2,21 +2,42 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
 const bookingController = require("../controllers/bookingsController");
+const authorizedRoles = require("../middleware/authorizedRole");
+const authenticateToken = require("../middleware/authMiddleware");
 
 router.post(
   "/createBooking",
+  authenticateToken,
+  authorizedRoles("user"),
   bookingController.createBooking
 );
 
-router.get("/getBookings", bookingController.getBookings);
-router.get("/getBookingsByUser/:user_id", bookingController.getBookingByUser);
+router.get(
+  "/getBookings",
+  authenticateToken,
+  authorizedRoles("user", "admin"),
+  bookingController.getBookings
+);
+router.get(
+  "/getBookingsByUser/:user_id",
+  authenticateToken,
+  authorizedRoles("user", "admin"),
+  bookingController.getBookingByUser
+);
 
 router.put(
   "/updateBooking/:id",
   upload.single("booking_image"),
+  authenticateToken,
+  authorizedRoles("user", "admin"),
   bookingController.updateBookings
 );
 
-router.delete("/deleteBooking/:id", bookingController.deleteBooking);
+router.delete(
+  "/deleteBooking/:id",
+  authenticateToken,
+  authorizedRoles("admin"),
+  bookingController.deleteBooking
+);
 
 module.exports = router;
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,12 +2,27 @@ const express = require("express");
 const router = express.Router();
 // const upload = require("../middleware/upload");
 const contactController = require("../controllers/contactController");
+const authorizedRoles = require("../middleware/authorizedRole");
+const authenticateToken = require("../middleware/authMiddleware");
 
-
-router.post("/createContact", contactController.createContact);
-router.get("/getContact", contactController.getContact);
-router.put("/updateContact/:id", contactController.updateContact);
-router.delete("/deleteContact/:id", contactController.deleteContact);
-
+router.post(
+  "/createContact",
+  authenticateToken,
+  authorizedRoles("user"),
+  contactController.createContact
+);
+router.get("/getContact", authenticateToken,authorizedRoles('admin'), contactController.getContact);
+router.put(
+  "/updateContact/:id",
+  authenticateToken,
+  authorizedRoles("admin"),
+  contactController.updateContact
+);
+router.delete(
+  "/deleteContact/:id",
+  authenticateToken,
+  authorizedRoles("admin"),
+  contactController.deleteContact
+);
 
 module.exports = router;
